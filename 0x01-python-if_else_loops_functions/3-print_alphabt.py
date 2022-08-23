56f3ff837853588f63bfa6264e4bb608c57a836a
favour-22 <anointed657@gmail.com>
2022-08-23 23:05:50 +0000

    Updated task Hexadecimal printing

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-    if ch != 101 and ch != 113:
-        print("{:c}".format(ch), end='')
+for i in range(0, 99):
+    print('{:d} = {}'.format(i, hex(i)))
