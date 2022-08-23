7b3346e1de143e8664d4edc838cb1b45a6924cc4
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-08-24 00:10:19 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print('{:d} = {}'.format(i, hex(i)))
+for ch in range(97, 123):
+    if ch != 101 and ch != 113:
+        print("{:c}".format(ch), end='')
