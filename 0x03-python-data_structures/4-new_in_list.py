0eea5824d4ceda7f65f8646cf1585af573d4a4b1
favour-22 <anointed657@gmail.com>
2022-08-29 11:04:43 +0000

    replace a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    id idx < 0 or idx >len(my_list) - 1:
+    if idx < 0 or idx >len(my_list) - 1:
         return my_list
     new_list = my_list[:idx]
     new_list.append(element)
