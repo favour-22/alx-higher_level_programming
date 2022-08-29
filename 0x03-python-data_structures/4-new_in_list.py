8e09aa0e9588bf84cdf24303a4262065cfa07f5e
favour-22 <anointed657@gmail.com>
2022-08-29 10:59:01 +0000

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    id idx < 0 or idx >len(my_list) - 1:
+        return my_list
+    new_list = my_list[:idx]
+    new_list.append(element)
+    new_list += my_list[idx + 1:]
+    return new_list
