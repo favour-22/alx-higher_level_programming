ffc39cc7274eb1ef0d442aabc2093a1bab4b7d99
favour-22 <anointed657@gmail.com>
2022-09-13 15:34:27 +0000

    Added comment to solution

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" Module containing the square Class"""
 class Square:
+"""The square class"""
     def __init__(self, size):
+"""
+Args:
+     size: The size of the Square instance
+"""
           self.__size = size
