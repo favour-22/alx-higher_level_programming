ce007d7c0952c1ec59cebb228aa25e2805baca4d
favour-22 <anointed657@gmail.com>
2022-09-14 15:11:16 +0000

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""This module defines a class Square:"""
+
 class Square:
-    """ A class that defines a square by its size
-    """
-    def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+    """Class for Square"""
+    def __init(self, size):
+      """initialize the class"""
+        self.__size = size
