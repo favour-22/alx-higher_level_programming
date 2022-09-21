29af4860c9a7096bb47f54a8ee75fe2c48d1a01f
favour-22 <anointed657@gmail.com>
2022-09-21 11:13:43 +0000

    file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module containing a function that prints a square 
+
+"""
+
+
+def print_square(size):
+    """function that prin ts a square using # 
+    
+    Agrs:
+         size (int): The size of one side of the square.
+    """
+
+    if size is not  int :
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be an integer")
+
+    print(('#' * size + '\n') * size, end='')
