697b71cdc45630c964ffd0a56155ca9e628163c0
favour-22 <anointed657@gmail.com>
2022-09-27 14:07:17 +0000

    append to the end

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that append a string at the end of a text file"""
+
+def append_write(filename="", text=""):
+    """append a string at the end of a text file"""
+
+    with open(filename, mode="a", encoding = "utf-8")as f:
+        return f.write(text)
