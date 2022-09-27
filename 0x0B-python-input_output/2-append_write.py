912d175b6940810bbefa3da76250727b1020aa9d
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-09-27 15:13:44 +0100

    fix errors

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 def append_write(filename="", text=""):
     """append a string at the end of a text file"""
 
-    with open(filename, mode="a", encoding = "utf-8")as f:
+    with open(filename, mode="a", encoding = "utf-8") as f:
         return f.write(text)
