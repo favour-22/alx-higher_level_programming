d1c4eee29ef42f3fa10b04ccb6f8d00e898231c1
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-09-27 21:22:50 +0100

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list and save them to file"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+file_name = "add_item.json"
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json_file(file_name)
+    except FileNotFoundError:
+        my_list = []
+    for i, arg in enumerate(sys.argv):
+        if i > 0:
+            my_list.append(arg)
+    save_to_json_file(my_list, file_name)#!/usr/bin/python3
+"""adds all arguments to a Python list and save them to file"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+file_name = "add_item.json"
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json_file(file_name)
+    except FileNotFoundError:
+        my_list = []
+    for i, arg in enumerate(sys.argv):
+        if i > 0:
+            my_list.append(arg)
+    save_to_json_file(my_list, file_name)
