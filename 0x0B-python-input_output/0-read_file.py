20add6ef7642e6a2bb48af5785dd8a0fbcf0fea7
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-09-27 14:06:08 +0100

    Read text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-A module to read a text file
+"""module to read a text file"""
 
-"""
 
 def read_file(filename=""):
-    """Reads a text file and prints it to standard output"""
-    with open("filename", encoding="utf-8") as f:
-        for text in f:
-            print(text, end="")
+    """reads a text file and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
