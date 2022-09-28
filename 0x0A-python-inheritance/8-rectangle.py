1dcdf36ca842363c1a251f8c0660ac95dc0622a1
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-09-28 21:12:47 +0100

    add solution

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""inherits from BaseGeometry"""
+
+
+class BaseGeometry:
+    """public instance method"""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialization of privates"""
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
