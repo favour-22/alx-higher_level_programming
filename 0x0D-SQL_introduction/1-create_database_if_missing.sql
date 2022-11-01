0f9a2e878603f456490d29fbcc4347a55451a411
favour-22 <anointed657@gmail.com>
2022-11-01 23:41:35 +0000

    case 2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
 -- Create a Database
---  Query to create the database hbtn_0c_0 in MySQL server
-CREATE database IF NOT EXISTS hbt_0c_0;
+
+-- Query to create the database hbtn_0c_0 in MySQL server
+CREATE database IF NOT EXISTS hbtn_0c_0;
