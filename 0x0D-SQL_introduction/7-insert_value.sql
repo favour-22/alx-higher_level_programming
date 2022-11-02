681933e81c73a4ad74e5f9b6d505c51dfc42de3b
favour-22 <anointed657@gmail.com>
2022-11-02 22:27:57 +0000

    added solution

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
 --Insert a new row in a table
-INSERT INTO first_table(ID, NAME) VALUES (89, "Best School");
+INSERT INTO first_tabl(id,name) VALUES (89, "Best School");
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
