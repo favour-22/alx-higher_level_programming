49bd7b33ad37b572140f97ee679239af1033e9bf
favour-22 <anointed657@gmail.com>
2022-11-02 00:40:18 +0000

    case 5

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,5 @@
 -- Creates a table in the current database
 
 -- Query to create a table called first_table in the current database in your MySQL server
-CREATE first_table IF NOT EXISTS (
-id INT, 
-name VARCHAR(256)
-);
+
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
