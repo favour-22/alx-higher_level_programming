a423728df23143d2cb80a25ec04d9a46e7ea421b
favour-22 <anointed657@gmail.com>
2022-11-02 00:29:06 +0000

    case 5

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table in the current database
+
+-- Query to create a table called first_table in the current database in your MySQL server
+CREATE first_table (
+id INT, 
+name VARCHAR(256));
