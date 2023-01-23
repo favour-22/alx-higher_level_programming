80d5df5fc168f27e36adc24db58266417383f167
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-01-23 21:45:43 +0100

    added solution

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
-    console.log(err);
-  } else {
-    console.log(contents);
+cconst fs = require('fs');
+
+function writeToFile(filePath, stringToWrite) {
+  try {
+    fs.writeFileSync(filePath, stringToWrite, 'utf8');
+  } catch (err) {
+    console.error(err);
   }
-});
+}
+
+if (process.argv.length === 4) {
+  const filePath = process.argv[2];
+  const stringToWrite = process.argv[3];
+  writeToFile(filePath, stringToWrite);
+}
